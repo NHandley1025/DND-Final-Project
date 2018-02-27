eb1e9ff26a9284f797ec7c7a56d31582b72adb3c
NHandley1025 <36775834+NHandley1025@users.noreply.github.com>
2018-02-27 15:08:13 -0700

    Revert "Revert "Add Main Page""
    This reverts commit d5b4b6b24f85e6d55039c15b1d4830a8363c7e42.

--- DNDFinalProject/src/gui/FxMain.java
+++ DNDFinalProject/src/gui/FxMain.java
@@ -4,26 +4,32 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.*;
-import javafx.stage.*;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class FxMain extends Application {
 
+	public static Stage stage;
+
 	@Override
 	public void start(Stage primaryStage) {
+		stage = primaryStage;
 		Parent root = null;
 		try {
-			root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			root = FXMLLoader.load(getClass().getResource("scenes/main/Main.fxml"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		Scene scene = new Scene(root, 300, 275);
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("scenes/main/mainstyle.css").toExternalForm());
 
-		primaryStage.setTitle("FXML Welcome");
+		primaryStage.setTitle("DnD Character Generator");
 		primaryStage.setScene(scene);
 		primaryStage.show();
+
 	}
 
 	public static void main(String[] args) {
