d019618facf1b131c6ffae0e9fb147e841460d22
Brevlin Thompson <bthompson@student.neumont.edu>
2018-02-28 14:29:52 -0700

    Updated spell package
    Set up model spell and added model-controller spells, also added new key for spell called spellKey

---
+++ DNDFinalProject/src/Spells/spellKey.java
@@ -0,0 +1,16 @@
+package Spells;
+
+public class spellKey {
+	public final int spellLevel;
+	public final String name;
+	public final String classReq;
+	
+	public spellKey(int spellLevel, String name, String classReq) {
+		super();
+		this.spellLevel = spellLevel;
+		this.name = name;
+		this.classReq = classReq;
+	}
+	
+	
+}
---
+++ DNDFinalProject/src/Spells/spells.java
@@ -0,0 +1,15 @@
+package Spells;
+
+import java.util.HashMap;
+
+public class spells {
+	private static HashMap<spellKey, spell> spells = new HashMap<>();
+	
+	public static spell get(spellKey key) {
+		
+	}
+	
+	public static void load() {
+		
+	}
+}
