33b32d62df7a598e90b15dbcbf74b1af866b8bd6
NHandley1025 <36775834+NHandley1025@users.noreply.github.com>
2018-02-28 13:33:35 -0700

    Add and Fix Displays

---
+++ DNDFinalProject/src/gui/Scenes/characterDisplay/CharacterDisplayController.java
@@ -0,0 +1,15 @@
+package gui.Scenes.characterDisplay;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class CharacterDisplayController implements Initializable{
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+
+}
