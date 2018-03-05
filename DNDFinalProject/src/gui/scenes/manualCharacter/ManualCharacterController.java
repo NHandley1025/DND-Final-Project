26457cbd32b8c16721090f69ec255c85184ebc02
Brevlin Thompson <bthompson@student.neumont.edu>
2018-03-05 14:22:36 -0700

    amended Barbarain and Skills

---
+++ DNDFinalProject/src/gui/scenes/manualCharacter/ManualCharacterController.java
@@ -0,0 +1,27 @@
+package gui.Scenes.manualCharacter;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class ManualCharacterController implements Initializable {
+
+	@FXML
+	public void exitButton(ActionEvent event) {
+		System.exit(0);
+	}
+
+	@FXML
+	public void genCharButton(ActionEvent event) {
+
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+
+	}
+
+}
