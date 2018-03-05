aad4f807d7f14587b8a83aad7e30882c1e797ce3
NHandley1025 <36775834+NHandley1025@users.noreply.github.com>
2018-03-05 15:00:18 -0700

    Changes

--- DNDFinalProject/src/gui/Scenes/manualCharacter/ManualCharacterController.java
+++ DNDFinalProject/src/gui/Scenes/manualCharacter/ManualCharacterController.java
@@ -3,19 +3,11 @@ package gui.Scenes.manualCharacter;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Separator;
 
 public class ManualCharacterController implements Initializable {
-
-	@FXML
-	ChoiceBox ClassChoiceBox;
-	@FXML
-	ChoiceBox RaceChoiceBox;
 
 	@FXML
 	public void exitButton(ActionEvent event) {
@@ -29,10 +21,7 @@ public class ManualCharacterController implements Initializable {
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		RaceChoiceBox.setItems(FXCollections.observableArrayList("Dragonborn", "Dwarf", "Elf", "Half Elf", "Halfling",
-				"Half Orc", "Human", "Tiefling"));
-		ClassChoiceBox.setItems(FXCollections.observableArrayList("Archetype", "Barbarian", "Bard", "Caster", "Class",
-				"Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"));
+
 	}
 
 }
