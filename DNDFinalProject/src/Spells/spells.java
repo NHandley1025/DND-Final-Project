636d39b2e50eff4c1918a0a8b16c272ed5a9b68a
Brevlin Thompson <bthompson@student.neumont.edu>
2018-03-06 13:30:27 -0700

    have spell controller read file

--- DNDFinalProject/src/Spells/spells.java
+++ DNDFinalProject/src/Spells/spells.java
@@ -1,5 +1,7 @@
 package Spells;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -24,7 +26,15 @@ public class spells {
 		return filteredSpells;
 	}
 	
-	public static void load() {
-		
+	public static void readSpellFile() throws IOException, FileNotFoundException{
+		String file = lib.ProgramUtil.readFile("Spells.txt");
+		String[] lines = file.split("\n");
+		for(int i=0; i<lines.length; i++) {
+			String[] things = lines[i].split("::");
+			if(things.length==4) {
+				spell spell = new spell(things[0], things[3], things[1], Integer.parseInt(things[2]));
+				spells.put(new spellKey(spell.spellLevel, spell.name, spell.classReq), spell);
+			}
+		}
 	}
 }
