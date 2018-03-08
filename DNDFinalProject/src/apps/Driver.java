4409ec274e11fc30e6313f4b40e36503229ad8d7
QuickCoyote <36782326+QuickCoyote@users.noreply.github.com>
2018-03-08 12:58:48 -0700

    c

--- DNDFinalProject/src/apps/Driver.java
+++ DNDFinalProject/src/apps/Driver.java
@@ -6,10 +6,7 @@ import gui.FxController;
 public class Driver {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		Generator.run();
-=======
->>>>>>> 61bb5a97a04941efa1ec7f4f349b2024d4b8ea07
 		FxController.launch(FxController.class, args);
 	}
 	
